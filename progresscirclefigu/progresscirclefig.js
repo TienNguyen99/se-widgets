e2815487cb9626328f2b8b30419040069965e011
TienNguyen99 <cuongem1008@gmail.com>
2024-02-19 15:37:34 +0700

    slime-anim

--- progresscirclefigu/progresscirclefig.js
+++ progresscirclefigu/progresscirclefig.js
@@ -1,31 +1,49 @@
-let circleprogress = document.querySelector(".circle"),
-    circlevalue = document.querySelector(".circle_value");
-    let start = 0,
-    step = 0,
-    end = 100,
-    speed = 20;
-    // function render(){
-    // let progress = setInterval(() => {
-    //   start = start + 1;
-    //   circlevalue.textContent = `${start}%`;
-    //   circleprogress.style.background = `conic-gradient(#FF9494 ${start *3.6}deg, #ededed 0deg`;
-    //   if (start == step){
-    //     clearInterval(progress);
-    //   }
+//Global state object
+let goal_total;
+let goal_amount;
 
-    // }, speed);
-    //   if (start >=end || start < 0){
-    //     clearInterval(progress);
-    //   }
-    // }
-function render(){
-      circlevalue.textContent = `${start}%`;
-      circleprogress.style.background = `conic-gradient(#FF9494 ${start *3.6}deg, #ededed 0deg`;
-}
-    
+let fieldData;
+let data;
 
-function updatebar(change){
-  start= start +change;
-  render();
+
+window.addEventListener('onWidgetLoad', function (obj) {
+    fieldData = obj.detail.fieldData;
+  	goal_total = fieldData.goal_total;
+  	goal_amount = fieldData.goal_amount;
+  	renderHTML(data);
+});
+window.addEventListener('onEventReceived', function (obj) {
+	const listener = obj.detail.listener;
+  	const event = obj["detail"]["event"];
+
+  // Listen to events based on user field settings
+  switch(listener){
+    case 'tip-latest':
+      goal_amount+= event["amount"];
+      debugger;
+      renderHTML(data);
+      break;
+  }
+});
+//runs code on event
+
+
+
+let renderHTML = (data) => {
+	const chartCircle = document.querySelector('.chart-circle');
+	const indicator = chartCircle.querySelector('.chart-indicator');
+  	const circlevalue = document.querySelector('.circle_value');
+  	
+  	
+  	let p = goal_amount / (goal_total) * 100;
+  	if(p<=100){
+    circlevalue.textContent = `${p}%`;
+  	chartCircle.style.setProperty('--p', `${p}%`);
+ 	indicator.style.setProperty('--p', `${p}%`);
+    }
+  	else{circlevalue.textContent = '100%';
+  	chartCircle.style.setProperty('--p', '100%');
+ 	indicator.style.setProperty('--p', '100%');}
+  	
   
-}
+};
