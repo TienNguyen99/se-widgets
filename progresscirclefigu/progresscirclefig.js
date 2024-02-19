9d9787073a3479f3e2b146da1305360b35ca0dae
TienNguyen99 <cuongem1008@gmail.com>
2024-02-19 19:41:20 +0700

    add_text

--- progresscirclefigu/progresscirclefig.js
+++ progresscirclefigu/progresscirclefig.js
@@ -1,7 +1,7 @@
 //Global state object
 let goal_total;
 let goal_amount;
-
+let follower_name= '';
 let fieldData;
 let data;
 
@@ -15,27 +15,41 @@ window.addEventListener('onWidgetLoad', function (obj) {
 window.addEventListener('onEventReceived', function (obj) {
 	const listener = obj.detail.listener;
   	const event = obj["detail"]["event"];
+  	
 
   // Listen to events based on user field settings
   switch(listener){
     case 'tip-latest':
       goal_amount+= event["amount"];
-      debugger;
+      renderHTML(data);
+      break;
+    case 'follower-latest':
+      goal_amount+= event["amount"];
+      follower_name = event["name"];
       renderHTML(data);
       break;
   }
 });
-//runs code on event
-
+//Function 
+function changeStatus(percent){
+  	const slimeImage = document.querySelector('.slime');
+	if (percent >=50){
+        slimeImage.src='https://tiennguyen99.github.io/se-widgets/assets/slime/jump-slime.jpg';
+        }else{
+        slimeImage.src='https://tiennguyen99.github.io/se-widgets/assets/slime/idle-slime.jpg';
+        }
+}
 
 
 let renderHTML = (data) => {
 	const chartCircle = document.querySelector('.chart-circle');
 	const indicator = chartCircle.querySelector('.chart-indicator');
   	const circlevalue = document.querySelector('.circle_value');
-  	
+  	const follower = document.querySelector('.follower_name');
+  	follower.textContent = "Xin chào"+`${follower_name}`;
   	
   	let p = goal_amount / (goal_total) * 100;
+  	changeStatus(p);
   	if(p<=100){
     circlevalue.textContent = `${p}%`;
   	chartCircle.style.setProperty('--p', `${p}%`);
